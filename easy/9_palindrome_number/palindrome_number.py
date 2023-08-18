9c16ea35f6ef8195ab51a6a0e3310d7090fd6fa6
Tom Lonergan <tomlonergan91@gmail.com>
2023-08-18 22:57:17 +0100

    day 12

---
+++ easy/9_palindrome_number/palindrome_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+
+print(Solution().isPalindrome(121))
+print(Solution().isPalindrome(-121))
+print(Solution().isPalindrome(10))
