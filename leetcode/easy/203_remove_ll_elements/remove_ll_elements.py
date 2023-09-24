a30e73974ffef2091dcd0bb7ac24578b72e741fc
Tom Lonergan <tomlonergan91@gmail.com>
2023-09-24 23:17:09 +0100

    day 49

---
+++ leetcode/easy/203_remove_ll_elements/remove_ll_elements.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        while head and head.val == val:
+            head = head.next
+        current = head
+        while current:
+            while current.next and current.next.val == val:
+                current.next = current.next.next
+            current = current.next
+        return head
+
+
+Solution().removeElements(ListNode(7, ListNode(7, ListNode(7, ListNode(7)))), 7)
